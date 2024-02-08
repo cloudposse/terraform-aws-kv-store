0c8d0adb1997262cccaa2c8963781bb77198bdb9
Matt Calhoun <matt@mattcalhoun.com>
2024-02-08 11:03:53 -0500

    update versions for linting

--- versions.tf
+++ versions.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    # Update these to reflect the actual requirements of your module
-    random = {
+    aws = {
       source  = "hashicorp/aws"
       version = ">= 4"
     }
